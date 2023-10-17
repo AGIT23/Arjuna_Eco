f986454231edf8f8d4a279a846700170369f7de5
AGIT23 <agitt.0223@gmail.com>
2023-10-17 11:25:30 +0700

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("back-button").addEventListener("click", function () {
+    // Anda dapat menambahkan kode di sini untuk mengarahkan pengguna kembali ke halaman sebelumnya atau melakukan tindakan lain sesuai kebutuhan.
+    alert("Anda kembali ke halaman sebelumnya.");
+});
+
+document.getElementById("community-form").addEventListener("submit", function (e) {
+    if (!document.getElementById("accept-terms").checked || !document.getElementById("acknowledge-consequences").checked) {
+        alert("Anda harus menyetujui syarat pengguna dan memahami konsekuensinya.");
+        e.preventDefault(); // Menghentikan pengiriman formulir jika syarat tidak terpenuhi.
+    } else {
+        // Anda dapat menambahkan kode di sini untuk mengirim data formulir ke server atau melakukan tindakan lainnya setelah pengguna mengklik tombol "Kirim".
+        alert("Data berhasil dikirim!");
+    }
+});
